5086adc76f565a1954c2088bd303053a8b9de4d9
Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>
2024-12-15 16:33:53 -0500

    added basic zip support to main

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,30 @@ mod zip;
 
 use crate::zip::zip_utils::zip_folder;
 use std::env;
+use std::path::Path;
 
 // Definition of messages as constants
 const UNKOWN_FLAG_MESSAGE: &str = "Unknown flag Use --help for usage information.";
 const HELP_MESSAGE: &str = r#"
 ferrumcrimper: A simple command-line tool for crimping files
+
+USEAGE:
+    FerrumCrimper [FLAGS] [infile] [outfile]
 First option must be a mode flag:
   --hello       Print 'Hello, World!'
-  --goodbye     Print 'Goodbye, World!'
-Useage:
-    FerrumCrimper [FLAGS] [infile] [outfile]
 OPTIONS:
   --license    Print the license information
   --help       Display this help message
+  --zip, -z    Zip a folder
 "#;
 const GNU_LICENSE_MESSAGE: &str = r#"
-Ferrum Crimper Version 0.0.1, 
-Copyright (C) 2024 Alan A. Aguilar, Kenneth A. Jenkins,
+Ferrum Crimper Version 0.0.1,
+
+Copyright (C) 2024 Alan D. Aguilar, Kenneth A. Jenkins,
 and contributors. License GPLv2+: GNU GPL version 2 or later.
 Ferrum Crimper comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
-under certain conditions; type `FerrumCrimper --help' for help."#;
+under certain conditions; type `fec --help' for help."#;
 
 fn main() {
     // Collect the command-line arguments into a vector
@@ -37,16 +40,30 @@ fn main() {
                 println!("Hello, World!");
             }
             "--license" => {
-                println!("{}", GNU_LICENSE_MESSAGE);
+                println!("{}", GNU_LICENSE_MESSAGE.trim_start());
             }
             "--help" | "-h" => {
-                println!("{}", HELP_MESSAGE);
+                println!("{}", HELP_MESSAGE.trim_start());
             }
             "--zip" | "-z" => {
-                println!("Zip file");
+                if args.len() < 3 {
+                    println!("Error: You must specify an input directory to zip.");
+                    return;
+                }
+                let input_dir = &args[2];
+                let path = Path::new(input_dir);
+
+                match zip_folder(path) {
+                    Ok(zipped_file) => {
+                        println!("Successfully zipped the folder.\n Output file: {:?}", zipped_file);
+                    }
+                    Err(e) => {
+                        println!("Failed to zip the folder:\n {}", e);
+                    }
+                }
             }
             _ => {
-                println!("{}", UNKOWN_FLAG_MESSAGE);
+                println!("{}", UNKOWN_FLAG_MESSAGE.trim_start());
             }
         }
     } else {
